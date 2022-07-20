c6c150c75b05ef651bc34e49cc1a1679215f5551
Richard Stock <rstock.co@gmail.com>
2022-07-20 03:16:15 +0000

    add voting-station challenge exercise

---
+++ voting-station.js
@@ -0,0 +1,26 @@
+// In order for a station to be deemed appropriate, it must have a capacity of at least 20, and be a school or community centre.
+
+const stations = [
+  ["Big Bear Donair", 10, "restaurant"],
+  ["Bright Lights Elementary", 50, "school"],
+  ["Moose Mountain Community Centre", 45, "community centre"],
+];
+
+const chooseStations = (stations) => {
+  let goodstations = [];
+  for (let i = 0; i < stations.length; i++) {
+    let stationName = stations[i][0];
+    let stationCapacity = stations[i][1];
+    let stationType = stations[i][2];
+
+    if (
+      stationCapacity >= 20 &&
+      (stationType === "school" || stationType === "community centre")
+    ) {
+      goodstations.push(stationName);
+    }
+  }
+  return goodstations;
+};
+
+chooseStations(stations);
