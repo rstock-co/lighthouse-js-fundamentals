674d1b5b34d9a477d0aa2956ca8030ebd3294f1f
Richard Stock <rstock.co@gmail.com>
2022-07-20 18:50:15 +0000

    add packing-list exercise

---
+++ ice-cream.js
@@ -0,0 +1,12 @@
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+let newIceCreamFlavours = iceCreamFlavours;
+newIceCreamFlavours.push('root beer');
+console.log(newIceCreamFlavours);
+
+console.log(newIceCreamFlavours[0]);
+
+console.log(newIceCreamFlavours[5]);
+
+console.log(newIceCreamFlavours.length);
