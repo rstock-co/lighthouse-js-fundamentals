09662a0475639c29e8417be3ffbfdb3d49e7a162
Richard Stock <rstock.co@gmail.com>
2022-07-20 21:13:24 +0000

    add looping-lastIndexOf exercise

---
+++ looping-lastIndexOf.js
@@ -0,0 +1,14 @@
+const lastIndexOf = (arr, val) => {
+  let ind = -1;
+  arr.forEach((element, index) => {
+    if (element === val) ind = index;
+  });
+  return ind;
+};
+
+console.log(lastIndexOf([0, 1, 4, 1, 2], 1), "=?", 3);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 2), "=?", 4);
+console.log(lastIndexOf([0, 1, 4, 1, 2], 3), "=?", -1);
+console.log(lastIndexOf([5, 5, 5], 5), "=?", 2);
+console.log(lastIndexOf([], 3), "=?", -1);
+console.log(lastIndexOf([5], 5), "=?", 0);
